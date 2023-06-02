246a85b4ca8c6b886eb8afad86746e05dd938377
amaders <112833243+amaders@users.noreply.github.com>
2023-06-02 09:39:29 -0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -6,60 +6,76 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.utilities import ApifyWrapper
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Pinecone
+from langchain.embeddings import OpenAIEmbeddings
+import pinecone
 import os
 
 os.environ["OPENAI_API_KEY"] = "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"
 os.environ["APIFY_API_TOKEN"] = "apify_api_vYzWRLX7dHb1H4qE7WjjsPhNPNoMf52QVzli"
 
-apify = ApifyWrapper()
+PINECONE_API_KEY = "38ab2900-421b-4c91-9aeb-90533fd173f7"
+PINECONE_ENV = "us-west4-gcp-free"
 
-loader = apify.call_actor(
-    actor_id="apify/website-content-crawler",
-    run_input={
-        "aggressivePrune": False,
-        "debugMode": False,
-        "htmlTransformer": "readableText",
-        "proxyConfiguration": {"useApifyProxy": True},
-        "removeElementsCssSelector": "header, nav, footer",
-        "saveFiles": False,
-        "saveHtml": False,
-        "saveMarkdown": False,
-        "saveScreenshots": False,
-        "startUrls": [{"url": "http://help.rupahealth.com/en/"}],
-        "textExtractor": "readableText",
-        "crawlerType": "playwright:chrome",
-        "maxCrawlDepth": 20,
-        "maxCrawlPages": 9999999,
-        "maxConcurrency": 200,
-        "initialConcurrency": 0,
-        "initialCookies": [],
-        "dynamicContentWaitSecs": 10,
-        "clickElementsCssSelector": '[aria-expanded="false"]',
-        "readableTextCharThreshold": 100,
-        "maxResults": 9999999,
-    },
-    dataset_mapping_function=lambda item: Document(
-        page_content=item["text"] or "", metadata={"source": item["url"]}
-    ),
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+
+embeddings = OpenAIEmbeddings()
+
+
+# apify = ApifyWrapper()
+
+# loader = apify.call_actor(
+#    actor_id="apify/website-content-crawler",
+#    run_input={
+#        "aggressivePrune": False,
+#        "debugMode": False,
+#        "htmlTransformer": "readableText",
+#        "proxyConfiguration": {"useApifyProxy": True},
+#        "removeElementsCssSelector": "header, nav, footer",
+#        "saveFiles": False,
+#        "saveHtml": False,
+#        "saveMarkdown": False,
+#        "saveScreenshots": False,
+#        "startUrls": [{"url": "http://help.rupahealth.com/en/"}],
+#        "textExtractor": "readableText",
+#        "crawlerType": "playwright:chrome",
+#        "maxCrawlDepth": 20,
+#        "maxCrawlPages": 9999999,
+#        "maxConcurrency": 200,
+#        "initialConcurrency": 0,
+#        "initialCookies": [],
+#        "dynamicContentWaitSecs": 10,
+#        "clickElementsCssSelector": '[aria-expanded="false"]',
+#        "readableTextCharThreshold": 100,
+#        "maxResults": 9999999,
+#    },
+#    dataset_mapping_function=lambda item: Document(
+#        page_content=item["text"] or "", metadata={"source": item["url"]}
+#    ),
+# )
+
+# index = VectorstoreIndexCreator().from_loaders([loader])
+
+text_field = "text"
+index_name = "langchain-apify-test"
+
+index = pinecone.Index(index_name)
+
+vectorstore = Pinecone(index, embeddings.embed_query, text_field)
+
+llm = ChatOpenAI(
+    openai_api_key="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",
+    model_name="gpt-3.5-turbo",
+    temperature=0.0,
 )
 
-index = VectorstoreIndexCreator().from_loaders([loader])
+qa = RetrievalQA.from_chain_type(
+    llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
+)
 
 
 def get_response(msg):
     query = msg
-    result = index.query_with_sources(query)
-
-    return result["answer"]
-
-
-if __name__ == "__main__":
-    print("Let's chat! (type 'quit' to exit)")
-    while True:
-        # sentence = "do you use credit cards?"
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
-
-        resp = get_response(sentence)
-        print(resp)
+    result = qa.run(query)
+    return result
