97f2b64fefb021c6ee3baf22d7e7be7c0ed57645
Theodore Calabrese (Desktop) <tcalabre@mit.edu>
2023-06-06 15:32:06 -0400

    finished implementing fullscreen mode

--- static/app.js
+++ static/app.js
@@ -2,6 +2,8 @@ class Chatbox {
     constructor() {
         this.args = {
             openButton: document.querySelector('.chatbox__button'),
+            fullscreenButton: document.querySelector('.fullscreen__button'),
+            fullscreenCloseButton: document.querySelector('.fullscreen__close'),
             chatBox: document.querySelector('.chatbox__support'),
             fullscreenChat: document.querySelector('.chatbox__fullscreen'),
             sendButton1: document.querySelectorAll('.send__button')[0],
@@ -10,16 +12,19 @@ class Chatbox {
 
         console.log(this.args);
 
-        this.state = false;
+        this.chatBoxState = false;
+        this.fullscreenState = false;
         this.messages = [];
     }
 
     display() {
-        const {openButton, chatBox, fullscreenChat, sendButton1, sendButton2} = this.args
+        const {openButton, fullscreenButton, fullscreenCloseButton, chatBox, fullscreenChat, sendButton1, sendButton2} = this.args
 
-        openButton.addEventListener("click", () => this.toggleState(chatBox))
-        sendButton1.addEventListener('click', () => this.onSendButton(chatBox, fullscreenChat))
-        sendButton2.addEventListener('click', () => this.onSendButton(chatBox, fullscreenChat))
+        openButton.addEventListener("click", () => this.toggleChatBoxState(chatBox));
+        fullscreenButton.addEventListener("click", () => this.toggleFullscreenState(fullscreenChat));
+        fullscreenCloseButton.addEventListener("click", () => this.toggleFullscreenState(fullscreenChat));
+        sendButton1.addEventListener('click', () => this.onSendButton(chatBox, fullscreenChat));
+        sendButton2.addEventListener('click', () => this.onSendButton(chatBox, fullscreenChat));
 
         const node1 = chatBox.querySelector('input');
         const node2 = fullscreenChat.querySelector('input');
@@ -37,16 +42,29 @@ class Chatbox {
         })
     }
 
-    toggleState(chatbox) {
+    toggleChatBoxState(chatbox) {
         
 
-        this.state = !this.state;
+        this.chatBoxState = !this.chatBoxState;
 
-        if (this.state) {
+        if (this.chatBoxState) {
             chatbox.classList.add('chatbox--active')
         }
         else {
             chatbox.classList.remove('chatbox--active')
+        }
+    }
+
+    toggleFullscreenState(fullscreeenChat) {
+        
+
+        this.fullscreenState = !this.fullscreenState;
+
+        if (this.fullscreenState) {
+            fullscreeenChat.classList.add('fullscreen--active')
+        }
+        else {
+            fullscreeenChat.classList.remove('fullscreen--active')
         }
     }
 
@@ -69,7 +87,8 @@ class Chatbox {
         this.messages.push(msg1);
         
         textField1.value = '';
-        this.updateChatText(chatbox);
+        textField2.value = '';
+        this.updateChatText(chatbox, fullscreenChat);
 
 
         fetch($SCRIPT_ROOT + '/predict' , {
@@ -88,7 +107,7 @@ class Chatbox {
             console.log(msg2.message);
 
             this.messages.push(msg2);
-            this.updateChatText(chatbox);
+            this.updateChatText(chatbox, fullscreenChat);
             textField1.value = '';
             textField2.value = '';
         }).catch((error) => {
@@ -101,7 +120,7 @@ class Chatbox {
          
     }
 
-    updateChatText(chatbox) {
+    updateChatText(chatbox, fullscreeenChat) {
         var html = '';
         console.log(this.messages);
         this.messages.slice().reverse().forEach(function(item,) {
@@ -115,11 +134,13 @@ class Chatbox {
             }
         });
 
+        const chatmessage1 = chatbox.querySelector('.chatbox__messages');
+        chatmessage1.innerHTML = html
+        const chatmessage2 = fullscreeenChat.querySelector('.chatbox__messages');
+        chatmessage2.innerHTML = html
 
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
-        chatmessage.innerHTML = html
 
-        console.log(chatmessage.innerHTML)
+        console.log(chatmessage1.innerHTML)
 
     }
 
@@ -129,7 +150,7 @@ const chatbox = new Chatbox()
 
 chatbox.display()
 
-
+/*
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -155,3 +176,4 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+*/
